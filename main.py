43f4be21c319323d1b91faf85b4e0441a52ac003
kmesousa <exoticbutters5xg@gmail.com>
2026-06-17 10:26:24 -0300

    inserir elementos do inventario no grid

--- main.py
+++ main.py
@@ -34,9 +34,13 @@ class Celula():
     def mudar(self):
         self.vazio = not self.vazio
         if not self.vazio:
-            self.cor = vermelho
+            self.cor = verde
         else:
             self.cor = azul
+            self.valor = None
+    def limpar(self):
+        self.valor = None
+        self.cor = azul
 
 class Board():
     def __init__(self, largura, altura, linhas, colunas, corBloco, corBorda):
@@ -77,7 +81,14 @@ class Board():
                 celula = self.identificar_cell(li, col)
                 xCell = x + (celula.largura + self.borda) * col
                 yCell = y + (celula.altura + self.borda) * li
-                pygame.draw.rect(screen, celula.cor, rect=((xCell, yCell, celula.largura, celula.altura)))
+                bloco = pygame.Rect((xCell, yCell, celula.largura, celula.altura))
+                pygame.draw.rect(screen, celula.cor, bloco)
+                if celula.valor:
+                    text = celula.valor
+                    text_surface = font.render(text, True, branco)
+                    text_rect = text_surface.get_rect()
+                    text_rect.center = bloco.center
+                    screen.blit(text_surface, text_rect)
     
     def selecionar_cell(self, pos):
             # converter as cordenadas x/y da tela para cordenadas do tabuleiro
@@ -89,10 +100,8 @@ class Board():
                 return self.identificar_cell(int(li), int(col))
 
 class Inventario(Board):
-    def selecionado(self, cell):
-        pass
     def render_inven(self, screen, x, y):
-        portas = ['AND', 'NOT']
+        portas = ['AND', 'NOT', 'IN', 'OUT']
         fios = ['horizontal', 'vertical', 'LtT', 'LtB', 'RtT', 'RtB']
         i = 0
         self.x = x
@@ -108,9 +117,13 @@ class Inventario(Board):
                 #escolher fio/porta
                 if i < len(fios):
                     text = fios[i]
-                else:
+                elif i < len(fios) + len(portas):
                     text = portas[i-len(fios)]
+                else:
+                    text = '--'
                 i+=1
+                #atualizar atributos na célula
+                celula.inserir_valor(text)
                 #renderizar texto em uma superfice
                 text_surface = font.render(text, True, branco)
                 #alinhas texto ao retantgulo 
@@ -120,20 +133,21 @@ class Inventario(Board):
                 pygame.draw.rect(screen, celula.cor, bloco)
                 #desenhar text surface em cima do retangulo
                 screen.blit(text_surface, text_rect)
-
+    def selecionado(self, cell):
+        pass
 
 #criando os objetos - grid
 grid = Board(900, 600, 11, 8, azul, preto)
 gridX = 20
 gridY = 30
 
-cell = grid.identificar_cell(0,0)
-cell.inserir_valor('AND')
-grid.identificar_cell(7,3).inserir_valor('NOT')
-grid.checar()
+# cell = grid.identificar_cell(0,0)
+# cell.inserir_valor('AND')
+# grid.identificar_cell(7,3).inserir_valor('NOT')
 
 #criando os objetos - inventário
-inven = Inventario(900, grid.identificar_cell(0,0).altura, 1, 8, azul, preto)
+inven = Inventario(1120, grid.identificar_cell(0,0).altura, 1, 10, azul, preto)
+selecionado = None
 
 #rodando o programa
 running = True
@@ -147,9 +161,14 @@ while running:
             pos = pygame.mouse.get_pos()
             # selecionar no board indicado
             if grid.selecionar_cell(pos):
-                grid.selecionar_cell(pos).mudar()
-            if inven.selecionar_cell(pos):
-                inven.selecionado(inven.selecionar_cell(pos))
+                if selecionado:
+                    grid.selecionar_cell(pos).inserir_valor(selecionado.valor)
+                else:
+                    grid.selecionar_cell(pos).mudar()
+            elif inven.selecionar_cell(pos):
+                selecionado = inven.selecionar_cell(pos)
+            else:
+                selecionado = None
 
     grid.render(screen, gridX, gridY)
     inven.render_inven(screen, gridX, gridY + grid.altura + 10)
